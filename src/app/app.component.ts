eb4aa340ffb8b046f17f9c7998f8f615edccbc0b
rahul.ranjan7201@gmail.com <rahul.ranjan7201@gmail.com>
2020-04-26 21:10:59 +0530

    Project Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import  {lorem} from 'faker';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'typing';
+  randomSentence : string = lorem.sentence();
+  enteredText:string = '';
+  onInput(value:string) {
+    this.enteredText = value
+  }
+  compare(randomLetter:string,enteredLetter:string) {
+    if(!enteredLetter) {
+      return 'pending';
+    }
+    return randomLetter === enteredLetter ? 'correct':'incorrect';
+  }
 }
